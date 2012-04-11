fbe8e7bfcd60ac028cf955f684a69d9c617cdc28
Alex Lauderdale <alauderdale@mac.com>
2012-04-11 14:51:42 -0600

    makintheapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'f9027973afa2a62cb5fd7a6d997cab07440fb34d4aa7fb6aed44efbbe75eaaf6097270d0d91a38d290f05c1a20ae22ef317331e6ed6c334e9a1acd71bb71b143'
